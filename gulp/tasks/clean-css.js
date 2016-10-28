b25364b1331f374558e3eb4a93bae8be9c38ef9d
Oksana Khristenko <oksana.khristenko@gmail.com>
2016-10-28 14:31:11 +0100

    Simplify clean-css task syntax

--- gulp/tasks/clean-css.js
+++ gulp/tasks/clean-css.js
@@ -2,10 +2,7 @@ var gulp = require('gulp');
 var clean = require('../helpers/clean').clean;
 var config = require('../config')();
 
-var cleanCssTask = (done) => {
-  var files = config.css.destFiles;
-  clean(files, done);
-};
+var cleanCssTask = done => clean(config.css.destFiles, done);
 
 gulp.task('clean-css', cleanCssTask);
 module.exports = cleanCssTask;
