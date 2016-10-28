8468b9c3b1b8dcf7ac106c9eb0a476e9c35afa61
Oksana Khristenko <oksana.khristenko@gmail.com>
2016-10-28 14:14:38 +0100

    Add postcss reporter to log CSS errors

--- gulp/tasks/css.js
+++ gulp/tasks/css.js
@@ -2,38 +2,40 @@ var gulp   = require('gulp');
 var autoprefixer = require('autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
 var postcss = require('gulp-postcss');
-var postcssImport = require('postcss-import')();
-var postcssNested = require('postcss-nested')();
-var postcssMixins = require('postcss-mixins')();
-var postcssConditionals = require('postcss-conditionals')();
-var postcssEach = require('postcss-each')();
-var postcssFor = require('postcss-for')();
-var postcssCustomMedia = require('postcss-custom-media')();
-var postcssCustomProperties = require('postcss-custom-properties')();
-var postcssCustomSelectors = require('postcss-custom-selectors')();
-var postcssCalc = require('postcss-calc')();
+var postcssReporter = require('postcss-reporter');
+var postcssImport = require('postcss-import');
+var postcssNested = require('postcss-nested');
+var postcssMixins = require('postcss-mixins');
+var postcssConditionals = require('postcss-conditionals');
+var postcssEach = require('postcss-each');
+var postcssFor = require('postcss-for');
+var postcssCustomMedia = require('postcss-custom-media');
+var postcssCustomProperties = require('postcss-custom-properties');
+var postcssCustomSelectors = require('postcss-custom-selectors');
+var postcssCalc = require('postcss-calc');
 var config = require('../config')();
 
 var cssTask = () => {
 
   var processors = [
-    postcssImport,
-    postcssNested,
-    postcssMixins,
-    postcssConditionals,
-    postcssEach,
-    postcssFor,
-    postcssCustomMedia,
-    postcssCustomProperties,
-    postcssCustomSelectors,
-    postcssCalc
+    postcssImport(),
+    postcssNested(),
+    postcssMixins(),
+    postcssConditionals(),
+    postcssEach(),
+    postcssFor(),
+    postcssCustomMedia(),
+    postcssCustomProperties(),
+    postcssCustomSelectors(),
+    postcssCalc(),
+    autoprefixer({ browsers: ['last 2 versions', '>5%'] }),
+    postcssReporter({clearMessages: true})
   ];
 
   return gulp
     .src(config.css.src)
     .pipe(sourcemaps.init())
     .pipe(postcss(processors))
-    .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions', '>5%'] }) ]))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest(config.css.destDir));
 };
